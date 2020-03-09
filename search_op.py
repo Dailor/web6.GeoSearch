6775b16b7c0d95a85bb61ff78dd408a8c0ac3db2
Damir <dadasay41@gmail.com>
2020-03-09 23:55:04 +0600

    добавил точку объекта

--- search_op.py
+++ search_op.py
@@ -11,8 +11,8 @@ API_KEY = {"static": "40d1649f-0493-4b70-98ba-98533de7710b",
 # python search.py Москва, ул. Ак. Королева, 12
 # Тогда запрос к геокодеру формируется следующим образом:
 
-# toponym_to_find = " ".join(sys.argv[1:])
-toponym_to_find = input()
+toponym_to_find = " ".join(sys.argv[1:])
+#toponym_to_find = input()
 
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
@@ -43,17 +43,21 @@ print(delta)
 map_params = {
     "ll": ",".join([toponym_longitude, toponym_lattitude]),
     "spn": delta,
-    "l": "map"
+    "l": "map",
+    'pt': f'{", ".join(toponym_coodrinates.split())}, pmwtm'.replace(' ', '')
 }
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 # ... и выполняем запрос
 response = requests.get(map_api_server, params=map_params)
 
+#Если не работает код показа картинки
 # with open('res.jpg', 'wb') as f:
 #     # f.write(BytesIO(response.content))
 #     f.write(response.content)
+
 # Создадим картинку
 # и тут же ее покажем встроенным просмотрщиком операционной системы
+
 Image.open(BytesIO(
     response.content)).show()
